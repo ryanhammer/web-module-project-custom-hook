c41617593f8701bd3d07d59cab5d95170810d977
rhammer35 <rhammer35@icloud.com>
2021-04-07 14:55:23 -0400

    called useLocalStorage in useDarkMode and used it to create custom hook for setting dark mode

--- src/hooks/useDarkMode.js
+++ src/hooks/useDarkMode.js
@@ -1,7 +1,9 @@
 import useLocalStorage from './hooks/useLocalStorage';
 
 const useDarkMode = () => {
+  const [darkMode, setDarkMode] = useLocalStorage('darkMode', false);
 
+  return [darkMode, setDarkMode];
 }
 
 export default useDarkMode;
